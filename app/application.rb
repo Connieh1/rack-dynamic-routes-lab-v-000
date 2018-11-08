38abdc47ecd94433dc5ed866803f71d16fd66651
Connie Hyman <constance.hyman@mchsi.com>
2018-11-08 21:19:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,10 @@ class Application
     req  = Rack::Request.new(env)
 
     @@items = []
-    
+
     if req.path.match(/items/)
       @@items.each do |item|
-        
+
       resp.write "#{item.price}\n"
     else
       resp.write "404 Route not found"
