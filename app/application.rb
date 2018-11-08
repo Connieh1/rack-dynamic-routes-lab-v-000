74f771aae6403207acf28ade8a4246e15bc77a19
Connie Hyman <constance.hyman@mchsi.com>
2018-11-08 21:51:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,16 +10,16 @@ class Application
 
 
     if req.path.match(/items/)
-      item_name =req.path.split("/items/").last
-
-      item = @@items.find{|i| i.name == item_name}
-      # if @@items.include?("#{item.name}")
-        resp.write item.price
-
-      # search_term = req.params["<Item Name>"]
-      # if @@items.include?(search_term)
-
-      # resp.write "#{search_term.price}\n"
+      # item_name =req.path.split("/items/").last
+      # 
+      # item = @@items.find{|i| i.name == item_name}
+      # # if @@items.include?("#{item.name}")
+      #   resp.write item.price
+      # 
+      # # search_term = req.params["<Item Name>"]
+      # # if @@items.include?(search_term)
+      # 
+      # # resp.write "#{search_term.price}\n"
 
     else
       resp.write "400"
