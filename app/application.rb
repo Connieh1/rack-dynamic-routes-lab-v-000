63a0ffab89839e1674f9a9e06fad705829482108
Connie Hyman <constance.hyman@mchsi.com>
2018-11-08 21:37:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 
     @@items = []
 
-    if req.path.match(/items/)
+    if req.path.match(/items/<ITEM NAME>)
       if @@items.include?("#{item.name}")
         resp.write "#{item.price}"
 
