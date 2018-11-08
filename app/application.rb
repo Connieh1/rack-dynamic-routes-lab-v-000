076429b24d509ea6a4b822e750dd7d183f41679f
Connie Hyman <constance.hyman@mchsi.com>
2018-11-08 22:12:04 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,10 @@
 class Application
 
   @@items = []
-
-
+  
   def call(env)
     resp = Rack::Response.new
     req  = Rack::Request.new(env)
-
-
 
     if req.path.match(/items/)
       item_name =req.path.split("/items/").last
@@ -21,9 +18,8 @@ class Application
     else
       resp.write "Route not found"
       resp.status = 404
-
-
     end
+    
     resp.finish
   end
 end
