9c7a0758db0461dbf42621976c11e42bb29af8be
Connie Hyman <constance.hyman@mchsi.com>
2018-11-08 21:52:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,19 +9,19 @@ class Application
 
 
 
-    if req.path.!match(/items/)
-      # item_name =req.path.split("/items/").last
-      #
-      # item = @@items.find{|i| i.name == item_name}
-      # # if @@items.include?("#{item.name}")
-      #   resp.write item.price
-      #
-      # # search_term = req.params["<Item Name>"]
-      # # if @@items.include?(search_term)
-      #
-      # # resp.write "#{search_term.price}\n"
+    if req.path.match(/items/)
+      item_name =req.path.split("/items/").last
 
-    # else
+      item = @@items.find{|i| i.name == item_name}
+      # if @@items.include?("#{item.name}")
+        resp.write item.price
+
+      # search_term = req.params["<Item Name>"]
+      # if @@items.include?(search_term)
+
+      # resp.write "#{search_term.price}\n"
+
+    else
       resp.write "400"
       resp.write "404"
       resp.write "Item not found"
